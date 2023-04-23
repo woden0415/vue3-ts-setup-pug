71797eb1a97893b0857a6629f3893a6e8e6a39a4
王冬 <wangdong7871@100.me>
2023-04-23 22:34:35 +0800

    feat: [vue3+ts+setup+pug]

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import ComponentHeader from "./component/component-header.vue";
+const count = ref(0)
+const data = reactive({
+  name: 'hello world',
+  age: 30,
+})
+const addCount = () => { 
+  count.value = count.value+1
+}
+</script>
+
+<script lang="ts">
+export default {
+  name: "AppIndex"
+}
+</script>
+
+<template lang="pug">
+div()
+  span() {{ count  }}
+  button(@click="addCount") addCount
+  hr
+  span() {{ data.name }}
+  span() {{ data.age }}
+  hr
+  ComponentHeader()
+
+</template>
+
+<style scoped></style>
---
+++ src/component/component-header.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+export default {
+  name: 'ComponentHeader'
+}
+</script>
+
+<template lang="pug">
+div() header
+</template>
